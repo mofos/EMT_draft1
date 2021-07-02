8f7d8449f63491291571d013fc0ce56181dbf320
mofos <isaacjohn2310@gmail.com>
2021-07-02 15:30:57 +0530

    image matrix done

--- js/body.js
+++ js/body.js
@@ -33,17 +33,5 @@
 			}
 	});	
 	
-	//Switch light/dark
-	
-	$("#switch").on('click', function () {
-		if ($("body").hasClass("dark")) {
-			$("body").removeClass("dark");
-			$("#switch").removeClass("switched");
-		}
-		else {
-			$("body").addClass("dark");
-			$("#switch").addClass("switched");
-		}
-	});  
 	
   })(jQuery);
